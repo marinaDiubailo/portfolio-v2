070fca3c16896db56bb507f25e6ba21dd606885d
Marina Diubailo <marinadiubailo@gmail.com>
2024-06-21 11:07:03 +0300

    feat: add Theme Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/styles/index.scss';
+import { ThemeProvider } from '@/app/providers';
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
@@ -15,8 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="data-theme"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
